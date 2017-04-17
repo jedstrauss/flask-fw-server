45187b77e24a1065baf069bd2c062fd324ad9374
Jed Strauss <jedstrauss@gmail.com>
2017-04-16 23:50:12 -0600

    Got rid of print debugging statements

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,11 @@ app = Flask(__name__, static_url_path=fw_path, static_folder=fw_folder)
 
 @app.route(br_path + '<filename>', methods=['PUT'])
 def br_put(filename):
-    print br_folder + filename
     try:
         with open(br_folder + filename, 'w+') as f:
             f.write(request.data)
     except:
         e = sys.exc_info()
-        print "Error: {0}".format(e)
         return "Internal Server Error\n",500
     else:
         return "OK\n",200
